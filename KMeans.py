b62d7ad31e2b320dceb5099f4199b55607527821
Dragos_Dinescu <dragos.dinescu@ulbsibiu.ro>
2020-10-21 19:52:13 +0300

    Added legend for clusters + now we can see the bottom clusters (some noise remains, will fix that soon)

--- KMeans.py
+++ KMeans.py
@@ -19,8 +19,37 @@ print(dataframe)
 kmeans = KMeans(n_clusters=4).fit(dataframe)
 centroids = kmeans.cluster_centers_
 print(centroids)
+
+dataframe['cluster'] = kmeans.labels_
+
+fig = plt.figure()
+ax = plt.axes(projection='3d')
+ax.scatter3D(dataframe['x'], dataframe['y'], dataframe['z'], c= kmeans.labels_.astype(float),alpha=0.3)
+ax.scatter3D(centroids[:, 0], centroids[:, 1],centroids[:, 2], c='red', s=50)
+
+print(dataframe.cluster.unique())
+fig = plt.figure()
+ax = plt.axes(projection='3d')
+for value in dataframe.cluster.unique():
+    value_df = dataframe[dataframe.cluster == value]
+    ax.scatter3D(value_df['x'], value_df['y'], value_df['z'], label=value)
+plt.title(value)
+plt.legend()
+plt.show()
+
+dataframe=dataframe[(dataframe['cluster'] != 1) & (dataframe['cluster'] != 3)]
+print (dataframe)
+kmeans = KMeans(n_clusters=10).fit(dataframe)
+centroids = kmeans.cluster_centers_
+print(centroids)
+
+dataframe['cluster'] = kmeans.labels_
+
 fig = plt.figure()
 ax = plt.axes(projection='3d')
 ax.scatter3D(dataframe['x'], dataframe['y'], dataframe['z'], c= kmeans.labels_.astype(float),alpha=0.3)
 ax.scatter3D(centroids[:, 0], centroids[:, 1],centroids[:, 2], c='red', s=50)
 plt.show()
+
+
+
